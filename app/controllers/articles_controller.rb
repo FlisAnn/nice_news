e0e6c93f33293921b595edd87021cd9012a9981b
flis_ann <ann.flismark@gmail.com>
2020-11-15 00:55:34 +0100

    Adding, changing and trying to get tests to pass. Test are not running green at the moment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,36 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @artcle = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params) 
+      redirect_to root_path, notice: 'Your article was successfully created'
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if
+      article_params.present? 
+      @article.update(article_params)
+      redirect_to @article, notice: 'Article was successfully updated' #if updated redirected back to article + message
+    else
+      redirect_to edit_article_path, notice: 'Something went wrong, try again' # other stay andshow error message
+    end
+  end
+  
+  private
+
+  def article_params # only allow title and content. Nothing is ok
+    params.require(:article).permit(:title, :content)
+  end
+
 end
+
