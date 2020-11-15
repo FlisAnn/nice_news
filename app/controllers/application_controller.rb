8d0b9d63361cf3e9313e85f36e7be811942c1215
flis_ann <ann.flismark@gmail.com>
2020-11-15 22:05:08 +0100

    Writing first test for user can sign up but it is not passing. Trying to add code to pass it. Trying to understand what has to be installed in order for everything to work. Adding controller sessions and user sessions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,19 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-def configure permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-  devise_parameter_sanitizer.permit(:account_update, keys: [:email]) 
-end
+  def configure permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email]) 
+  end
 
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize 
+    redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :email }
+  end
+  
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :email}
+    it { is_expected. to validate_presence_of :password}
+  end
+
+  describe 'Factory' do
+    it 'expects a valid Factory' do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+  end  
 end
