fedb1d99e1509e36933dd9b7287bb630e7c09dca
flis_ann <ann.flismark@gmail.com>
2020-11-15 19:21:35 +0100

    Creating a new branch and adding first test to see that user can sign up for account.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   add_flash_types :notice
+
+  protect_from_forgery with: :exception
+
+def configure permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:email]) 
+end
+
 end
